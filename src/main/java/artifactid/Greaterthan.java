57ec98900e3b5e985f146db842e9c8dca6debdbe
Anirudha Gadekar <AnirudhaGadekarag@gmail.com>
2024-12-24 10:14:43 +0530

    Initial commit

---
+++ src/main/java/artifactid/Greaterthan.java
@@ -0,0 +1,35 @@
+package artifactid;
+
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+public class Greaterthan {
+	
+
+	
+		public static void main(String[] args) {
+//			System.out.println(1);
+			Configuration cfg=new Configuration();
+//			System.out.println(2);
+			cfg.configure();
+//			System.out.println(3);
+			cfg.addAnnotatedClass(Student.class);
+			SessionFactory sessionFactory =cfg.buildSessionFactory();
+//			System.out.println(4);
+			Session session=sessionFactory.openSession();
+			Criteria criteria=session.createCriteria(Student.class);
+			criteria.add(Restrictions.gt("age", 40));
+			
+			List<Student>list=criteria.list();
+			for(Student student :list) {
+				System.out.println(student);
+
+}	
+}
+}
